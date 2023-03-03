9d8d5e4148dfce811313861396d26bd91ec15fcf
fred belotte <fredbelotte@outlook.com>
2023-03-03 17:28:36 -0600

    add logger to uncaughtException, unhandledRejection processes

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,10 +1,19 @@
 "use strict";
 
 import { createServer } from "node:http";
+import { logger } from "./src/logger.js";
 import { server } from "./src/server.js";
 
 const HTTP_PORT = process.env.HTTP_PORT || 3000;
 
+process.on("uncaughtException", (err) => {
+  logger.error(err);
+});
+
+process.on("unhandledRejection", (err) => {
+  logger.error(err);
+});
+
 createServer(server).listen(HTTP_PORT, () => {
-  console.log(`server running at [:${HTTP_PORT}]`);
+  logger.info(`server running at [:${HTTP_PORT}]`);
 });

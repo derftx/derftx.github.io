366797c6b1aca3f61a02624e3cc992cb83cf0efc
fred belotte <fredbelotte@outlook.com>
2023-03-06 22:12:18 -0600

    add logger to program file

--- dotnet/derftx.service/program.cs
+++ dotnet/derftx.service/program.cs
@@ -1,7 +1,23 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Serilog;
 using derftx.service;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+Logger.UseSerilog();
+
+app.UseExceptionHandler((handler) => {
+  handler.Run(async (context) => {
+    var errorHandler = context.Features.Get<IExceptionHandlerPathFeature>();
+
+    Log.Error(errorHandler?.Error.Message ?? "application error");
+
+    await context.Response.WriteAsync("server error");
+  });
+});
+
+Log.Information("program file");
 
 app.Run(Server.Root);
 
